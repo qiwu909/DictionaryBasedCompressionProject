f4024de6a422c7feebd9a9ecc4d458b5921ce89e
qiwu909 <90415258+qiwu909@users.noreply.github.com>
2022-11-01 16:43:11 -0400

    Update DictionaryBasedCompression.java

--- src/main/java/DictionaryBasedCompression.java
+++ src/main/java/DictionaryBasedCompression.java
@@ -1,5 +1,10 @@
-public class DictionaryBasedCompression {
-  public String toLowerCase(String s) {
+import java.io.*;
+
+public class datacompression {
+    public datacompression(){
+
+    }
+    public String toLowerCase(String s) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < s.length(); ++i) {
             char ch = s.charAt(i);
@@ -10,11 +15,24 @@ public class DictionaryBasedCompression {
         }
         return sb.toString();
     }
-   public String RemovePunctuations(String str){
-       str = str.replaceAll("\\p{Punct}","");
-       return str;
-   }
-   public static void main(String[] args){
+    public String RemovePunctuations(String str){
+        str = str.replaceAll("\\p{Punct}","");
+        return str;
+    }
+    public void histogram(){
 
-   }
+    }
+    public static void main(String[] args) throws IOException {
+        File a = new File("src/1.txt");
+        BufferedReader read = new BufferedReader(new FileReader(a));
+        datacompression x = new datacompression();
+        String str = read.readLine();
+        while(str!=null){
+            str=x.toLowerCase(str);
+            str=x.RemovePunctuations(str);
+            System.out.println(str);
+            str=read.readLine();
+        }
+        read.close();
+    }
 }
