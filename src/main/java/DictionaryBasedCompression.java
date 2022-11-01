6b29af3289511d2850adcc1e43988daabe656e73
qiwu909 <90415258+qiwu909@users.noreply.github.com>
2022-11-01 16:22:02 -0400

    Update DictionaryBasedCompression.java

--- src/main/java/DictionaryBasedCompression.java
+++ src/main/java/DictionaryBasedCompression.java
@@ -1,2 +1,20 @@
 public class DictionaryBasedCompression {
+  public String toLowerCase(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); ++i) {
+            char ch = s.charAt(i);
+            if (ch >= 65 && ch <= 90) {
+                ch |= 32;
+            }
+            sb.append(ch);
+        }
+        return sb.toString();
+    }
+    public String RemovePunctuations(String str){
+        str = str.replaceAll("\\p{Punct}","");
+        return str;
+    }
+  public static void main(String[] args){
+
+    }
 }
